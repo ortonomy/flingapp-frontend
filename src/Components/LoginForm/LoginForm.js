4009a3c8fa8a8483c0b0722e0fb06b71d42af9b6
Jared Kobos <jaredkobos@gmail.com>
2017-10-02 20:08:08 -0400

    Use fixed height for login box

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -13,9 +13,9 @@ class LoginForm extends Component {
         return (
             <form className="LoginForm">
               <FormInputAnimated name="email" text="Email" />
-              <Feedback message="Invalid email address"/>
+              <Feedback message=""/>
               <FormInputAnimated name="password" text="Password" />
-              <Feedback message="Did you forget your email address? "/>
+              <Feedback message=""/>
               <a className="form-link">I've forgotten my password.</a>
               <div className="form-element btn-primary">LOGIN</div>
             </form>
