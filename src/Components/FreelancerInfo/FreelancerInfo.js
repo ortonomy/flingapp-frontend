f5d5aae9532fda1f0ad43cdd878e39b6a6ed9e24
Jared Kobos <jaredkobos@gmail.com>
2017-10-08 20:22:13 -0400

    Complete InfoTable component

--- src/Components/FreelancerInfo/FreelancerInfo.js
+++ src/Components/FreelancerInfo/FreelancerInfo.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import InfoTable from '../InfoTable/InfoTable';
 import './FreelancerInfo.css';
 
+const info = {
+  'first name': 'Jorge',
+  'last name': 'Brock'
+}
+
 class FreelancerInfo extends Component {
   render() {
     return (
       <div className='FreelancerInfo'>
-        <InfoTable title='Personal Information' />
-        <InfoTable title='Professional Information' />
-        <InfoTable title='Experience'/>
+        <InfoTable title='Personal Information' info={info}/>
+        <InfoTable title='Professional Information' info={info}/>
+        <InfoTable title='Experience' info={info}/>
       </div>
     )
   }
--- src/Components/InfoTable/InfoTable.js
+++ src/Components/InfoTable/InfoTable.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './InfoTable.css';
 
-function InfoTable (props) {
+const InfoTable = (props) => {
+  const tableData = []
+  for (let [key,value] of Object.entries(props.info)) {
+    const row = <tr><td className="table-key">{key}</td><td className="table-value">{value}</td></tr>
+    console.log(row)
+    tableData.push(row)
+  }
   return (
-    <div className="InfoTable">
-
+    <div className='InfoTable'>
+      <div className='info-title'>{props.title}</div>
+      <table className='info-table'>
+        {tableData}
+      </table>
     </div>
   )
 }
