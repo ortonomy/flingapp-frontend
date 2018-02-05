469a900661f0ebed2dcc331eabffce06462c0fa4
ortonomy <git@ortonomy.co>
2018-02-05 17:39:22 +0800

    adds styling and boilerplate code for /enroll route

--- src/containers/OrgEnrolment/component.js
+++ src/containers/OrgEnrolment/component.js
@@ -1,13 +1,27 @@
 // react
 import React, { Component } from 'react';
 
+//library dependencies
+import { Link } from 'react-router-dom';
+
 // styles
 import styles from './style.module.css';
+
+// copy and assets
+import logo from '../../assets/images/full_title_compact_trans@svg.svg';
 
 class OrgEnrolment extends Component {
   render() {
     return (
       <div className={styles.Body}>
+        <div className={styles.Content}>
+          <div className={styles.Image}>
+              <Link to="/"><img src={logo} alt="Fling" /></Link>
+          </div>
+          <div className={styles.Components} >
+            <p>Some Content</p>
+          </div>
+        </div>
       </div>
     );
   }
