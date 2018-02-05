1cb8e745e7a1d584a25bca49fefac34c588242e5
ortonomy <git@ortonomy.co>
2018-02-05 17:23:01 +0800

    issue #21 - adds OrgEnrolment component and new route /enroll

---
+++ src/containers/OrgEnrolment/component.js
@@ -0,0 +1,16 @@
+// react
+import React, { Component } from 'react';
+
+// styles
+import styles from './style.module.css';
+
+class OrgEnrolment extends Component {
+  render() {
+    return (
+      <div className={styles.Body}>
+      </div>
+    );
+  }
+}
+
+export default OrgEnrolment;
